1f6c57fb8b0d7d3c404c11a136748fd55b2d9370
RenkasVajra <twinka4ik174@mail.ru>
2024-06-08 15:06:55 +0500

    add requirements

--- CinemaService/CinemaService/settings.py
+++ CinemaService/CinemaService/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ymyw86f8pmg9ln
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
